a46a87715fd12f950fc0c8d7e895a4a8c39b6650
thealamenthed <dalila.boualemfr@gmail.com>
2022-06-13 22:24:34 +0200

    clean architecture

---
+++ partials/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La cantine asiatique</title>
+    <!-- La feuille de style -->
+    <link rel="stylesheet" href="assets/style.css" />
+    <!-- Les polices d'écriture en provenance de  -->
+
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <a href="#" class="logo"><img src="assets/img/La cantine.png"></a>
+            <li><a class="active" href="index.php">Acceuil</a></li>
+            <li><a href="#"> Japon</a></li>
+            <li><a href="#"> Corée</a></li>
+            <li><a href="#"> Vietnam</a></li>
+            <li><a href="#"> Thailande</a></li>
+            <li><a href="#"> Malaisie</a></li>
+            <li><a href="#"> Chine</a></li>
+        </ul>
+    </nav>
